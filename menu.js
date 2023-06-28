0ca6829854e2b3447c7f7e78e027165dcb79c527
koonukaame <marialezhebokova@yandex.ru>
2023-06-28 18:01:21 +0300

    feat: add closing menu on link click

--- menu.js
+++ menu.js
@@ -1,9 +1,18 @@
 const button = document.querySelector('.nav__burger');
 const menu = document.querySelector('.nav__list');
+const link = document.querySelectorAll('.link');
 
 function toggleMenu() {
   menu.classList.toggle('_open');
   button.classList.toggle('nav__burger_open');
 }
 
+function closeMenuOnLinkClick() {
+  menu.classList.remove('_open');
+  button.classList.remove('nav__burger_open');
+}
+
 button.addEventListener('click', toggleMenu);
+link.forEach(link => {
+  link.addEventListener('click', closeMenuOnLinkClick)
+});
